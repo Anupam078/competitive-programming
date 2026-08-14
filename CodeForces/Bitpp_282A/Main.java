f9feda910882264ef97bf0664e7b636ec271c675
Anupam Ojha <anupamojha078@gmail.com>
2026-08-15 02:49:04 +0530

    Solved Bit++_282A of codeForces

--- CodeForces/Bitpp_282A/Main.java
+++ CodeForces/Bitpp_282A/Main.java
@@ -1,4 +1,25 @@
 package CodeForces.Bitpp_282A;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int X=0;
+        for(int i=0;i<n;i++){
+            String stmt = sc.next();
+            if (stmt.equals("++X")) {
+                ++X;
+            } else if (stmt.equals("X++")) {
+                X++;
+            } else if (stmt.equals("X--")) {
+                X--;
+            } else {
+                --X;
+            }
+        }
+        System.out.println(X);
+    }
 }
