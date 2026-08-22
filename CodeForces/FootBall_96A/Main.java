e4de560627a49b2f7019df10c6db45241e4df9d8
Anupam Ojha <anupamojha078@gmail.com>
2026-08-22 14:20:52 +0530

    Solved 96A on CodeForces

---
+++ CodeForces/FootBall_96A/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String pos=sc.next();
+        int result=0;
+        int count=0;
+        for(int i=1 ; i<pos.length(); i++){
+            if(pos.charAt(i)==pos.charAt(i-1)){
+                count++;
+            }else{
+                count=0;
+            }
+            result=Math.max(result,count);
+        }
+        if(result>=6){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
