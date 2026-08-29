7eae6ab2ea289d5103d296a5e24b1e65cbbe82c1
Anupam Ojha <anupamojha078@gmail.com>
2026-08-30 02:00:31 +0530

    Solved Lower and upper Bound via Binary Search Algorithm

---
+++ Algorithms/Binary_Search/lowerAndUpperBound.java
@@ -0,0 +1,39 @@
+package Algorithms.Binary_Search;
+
+public class lowerAndUpperBound {
+    public static int lowerBound(int [] nums ,int n){
+        int ans = nums.length;
+        int high = nums.length-1;
+        int low=0;
+        int mid = low + ((high-low)/2);
+        while(high>=low){
+            mid = low + ((high-low)/2);
+            if (nums[mid]>=n){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static int upperBound(int [] nums , int n){
+        int ans = nums.length;
+        int high = nums.length-1;
+        int low=0;
+        int mid = low + ((high-low)/2);
+        while(high>=low){
+            mid = low + ((high-low)/2);
+            if (nums[mid]>n){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
