6c6f63a4e1252251e6379f7ae854fa145e3587dc
Anupam Ojha <anupamojha078@gmail.com>
2026-08-30 16:06:41 +0530

    Solved Leetcode 35 Search Index Position via Binary Search implementing lower bound logic

---
+++ Algorithms/Binary_Search/SearchIndexPosition_35.java
@@ -0,0 +1,21 @@
+package Algorithms.Binary_Search;
+
+public class SearchIndexPosition_35 {
+    public static int searchIndex(int [] nums , int n){
+        int ans = nums.length;
+        int high = nums.length-1;
+        int low=0;
+        int mid = low + ((high-low)/2);
+        while(high>=low){
+            mid = low + ((high-low)/2);
+            if (nums[mid]>=n){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
